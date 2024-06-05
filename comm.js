6b6a70cb234952e522eb8acd482042135ef69811
Eyad Ashraf <163561140+eyadwebdeveloper@users.noreply.github.com>
2024-06-05 13:59:48 +0300

    Update comm.js

--- comm.js
+++ comm.js
@@ -1,12 +1,19 @@
 let inputTextAreaw1 = document.getElementById("input-textareaw1");
 let wordCountw1 = document.getElementById("word-countw1");
 
+let inputTextAreaw2 = document.getElementById("input-textareaw2");
+let wordCountw2 = document.getElementById("word-countw2");
+
 
 inputTextAreaw1.addEventListener("input", () => {
   let txt = inputTextAreaw1.value.trim();
   wordCountw1.textContent = txt.split(/\s+/).filter((item) => item).length + ' /150';
 });
 
+inputTextAreaw2.addEventListener("input", () => {
+  let txt = inputTextAreaw2.value.trim();
+  wordCountw1.textContent = txt.split(/\s+/).filter((item) => item).length + ' /300';
+});
 
 
 function textAreaAdjust(element) {
